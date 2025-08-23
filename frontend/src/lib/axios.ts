3c1549f33e0014c8ac01fdc80647d1a02fea0cd5
Mayur Patil <Mayurpatilofficial10@gmail.com>
2025-08-23 14:48:07 +0530

    Pushed new code1

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:4000/api',
+  baseURL: import.meta.env.VITE_API_BASE || 'https://birthday-song-generator-2.onrender.com/api',
 });
 
 export default api;
